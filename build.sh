aae57340a53090bd4e67d18e97d4d48547c006a0
Marc Bilodeau <marc.bilodeau@plixer.com>
2016-04-13 12:08:04 -0400

    updated compile directory

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 PLIXER_BASE=~/repos/ipfixify
 
 cd $PLIXER_BASE
-mkdir -p bin/
+mkdir -p bin/CentOS_x86_64
 make clean
-rm -f bin/ipfixify.exe
+rm -f bin/CentOS_x86_64/ipfixify.exe
 perl Makefile.PL
 make
 rm version.info
-
+mv -f bin/ipfixify.exe bin/CentOS_x86_64/ipfixify.exe
